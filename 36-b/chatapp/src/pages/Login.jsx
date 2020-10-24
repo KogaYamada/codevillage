0fe98e84e34597914993f5f11d724ef31dd3efcf
KogaYamada <koga0087y@gmail.com>
2020-10-24 14:51:23 +0900

    10/24 36-b期生

--- 36-b/chatapp/src/pages/Login.jsx
+++ 36-b/chatapp/src/pages/Login.jsx
@@ -1,9 +1,9 @@
-import React, {useState, useContext} from 'react';
+import React, { useState, useContext } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { TextField, Button } from '@material-ui/core';
 import { Link, useHistory, Redirect } from 'react-router-dom';
-import {auth} from "../config/firebase"
-import {AuthContext} from "../context/AuthService"
+import { auth } from '../config/firebase';
+import { AuthContext } from '../context/AuthService';
 
 const useStyles = makeStyles({
   title: {
@@ -22,47 +22,49 @@ const useStyles = makeStyles({
 // signupページで作成したユーザーでログインする
 
 const Login = () => {
-	const [email, setEmail] = useState('')
-	const [password, setPassword] = useState('')
-	const classes = useStyles();
-	const history = useHistory()
-	const user = useContext(AuthContext)
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const classes = useStyles();
+  const history = useHistory();
+  const user = useContext(AuthContext);
 
-	const handleSubmit = (e) => {
-		e.preventDefault()
-		auth.signInWithEmailAndPassword(email, password)
-			.then(()=>{
-				history.push("/")
-			}).catch((error)=>{
-				console.error("ログイン失敗", error)
-			})
-	}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        history.push('/');
+      })
+      .catch((error) => {
+        console.error('ログイン失敗', error);
+      });
+  };
 
-	if(user) {
-	  return	<Redirect to="/" />
-	}
-	
+  if (user) {
+    return <Redirect to='/' />;
+  }
+
   return (
     <form onSubmit={handleSubmit} className={classes.form}>
       <h1 className={classes.title}>ログインページ</h1>
       <TextField
-				type="email"
-				label="メールアドレス"
-				variant="outlined"
-				value={email}
-				onChange={(e)=> setEmail(e.target.value)}
-			/>
+        type='email'
+        label='メールアドレス'
+        variant='outlined'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
       <TextField
-				type="password"
-				label="パスワード"
-				variant="outlined"
-				value={password}
-				onChange={(e)=> setPassword(e.target.value)}
-			/>
-      <Button type="submit" variant="contained" color="secondary">
+        type='password'
+        label='パスワード'
+        variant='outlined'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button type='submit' variant='contained' color='secondary'>
         ログイン
       </Button>
-      <Link to="/signup">アカウントをお持ちでない方</Link>
+      <Link to='/signup'>アカウントをお持ちでない方</Link>
     </form>
   );
 };
--- 36-b/chatapp/src/pages/Room.jsx
+++ 36-b/chatapp/src/pages/Room.jsx
@@ -1,21 +1,83 @@
-import React, {useContext} from 'react';
-import {auth} from "../config/firebase"
-import {useHistory} from "react-router-dom"
-import {AuthContext}from "../context/AuthService"
+import React, { useContext, useEffect, useState } from 'react';
+import { auth, db } from '../config/firebase';
+import { useHistory } from 'react-router-dom';
+import { AuthContext } from '../context/AuthService';
+
+import { makeStyles } from '@material-ui/core/styles';
+import { Button, Card, TextField, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  messaage: {
+    padding: '15px',
+    margin: '15px',
+  },
+});
 
 const Room = () => {
-	const history = useHistory()
-	const user = useContext(AuthContext)
-	const logout = () => {
-		auth.signOut()
-		history.push("/login")
-	}
+  const history = useHistory();
+  const classes = useStyles();
+  const user = useContext(AuthContext);
+  const [messages, setMessages] = useState([]);
+  const [text, setText] = useState('');
+
+  // firestoreにメッセージの内容を追加
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    db.collection('messages')
+      .add({
+        content: text,
+        username: user.displayName,
+        createdAt: new Date(),
+      })
+      .then(() => {
+        console.log('追加成功');
+      })
+      .catch((error) => {
+        console.error('追加失敗', error);
+      });
+  };
+
+  // 最初の1回実行
+  useEffect(() => {
+    db.collection('messages').onSnapshot((querySnapshot) => {
+      // firestoreのmessagesコレクションの値が更新された時
+      const messages = querySnapshot.docs.map((doc) => {
+        return doc.data();
+      });
+      setMessages(messages);
+    });
+  }, []);
+
+  const logout = () => {
+    auth.signOut();
+    history.push('/login');
+  };
   return (
-	<>
-		<h1>Room</h1>
-		<button onClick={logout}>ログアウト</button>
-	</>
-	);
+    <>
+      <h1>Room</h1>
+      {messages.map((message, index) => {
+        return (
+          <Card key={index} className={classes.messaage}>
+            <Typography>メッセージ投稿者:{message.username}</Typography>
+            <Typography>{message.content}</Typography>
+          </Card>
+        );
+      })}
+      <form onSubmit={handleSubmit}>
+        <TextField
+          variant='outlined'
+          size='small'
+          placeholder='メッセージを入力'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <Button type='submit' variant='contained' color='secondary'>
+          送信
+        </Button>
+      </form>
+      <button onClick={logout}>ログアウト</button>
+    </>
+  );
 };
 
 export default Room;
