4aed4c989382158f648333bfa854d3692418f97f
KogaYamada <koga0087y@gmail.com>
2020-10-24 17:06:22 +0900

    10/24 38期生

--- 38/chatapp/src/pages/Room.jsx
+++ 38/chatapp/src/pages/Room.jsx
@@ -1,32 +1,84 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { auth, db } from "../firebase";
-import { AuthContext } from "../components/AuthService"
+import { auth, db } from '../firebase';
+import { AuthContext } from '../components/AuthService';
+import { Button, TextField, Card, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+// import firebase from "../firebase"
+
+const useStyles = makeStyles({
+  message: {
+    padding: '15px',
+    margin: '15px',
+  },
+});
 
 const Room = () => {
-	const [messages, setMessages] = useState([])
-	const user = useContext(AuthContext);
-	
+  const [messages, setMessages] = useState([]);
+  const [text, setText] = useState('');
+  const user = useContext(AuthContext);
+  const classes = useStyles();
+
   const signOut = () => {
     auth.signOut();
-	};
+  };
 
-	useEffect(()=>{
-		db.collection("messages").get()
-			.then((querySnapshot)=>{
-				setMessages(querySnapshot.docs.map((doc) => {
-					return {...doc.data(), id: doc.id}
-				}))
-			})
-	},[])
+  // メッセージを追加
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    db.collection('messages').add({
+      content: text,
+      user: user.displayName,
+      createdAt: new Date(),
+    });
+    /*
+	firebase.firestore().collection("messages").add({})
+	*/
+  };
+
+  //最初の一回
+  useEffect(() => {
+    // onSnapshot
+    // firestoreのmessagesコレクションのデータに更新があった時に実行される
+    db.collection('messages').onSnapshot((querySnapshot) => {
+      const messages = querySnapshot.docs.map((doc) => {
+        return { ...doc.data(), id: doc.id };
+      });
+      setMessages(messages);
+    });
+    // db.collection('messages')
+    //   .get()
+    //   .then((querySnapshot) => {
+    //     setMessages(
+    //       querySnapshot.docs.map((doc) => {
+    //         return { ...doc.data(), id: doc.id };
+    //       })
+    //     );
+    //   });
+  }, []);
 
   return (
     <>
       <h1>チャットルーム</h1>
-			<ul>
-				{messages.map((message) => {
-					return <li key={message.id}>メッセージ: {message.content} 投稿者: {message.user}</li>
-				})}
-			</ul>
+      {messages.map((message) => {
+        return (
+          <Card key={message.id} className={classes.message}>
+            <Typography>投稿者: {message.user}</Typography>
+            <Typography>{message.content}</Typography>
+          </Card>
+        );
+      })}
+      <form onSubmit={handleSubmit}>
+        <TextField
+          variant='outlined'
+          size='small'
+          placeholder='メッセージを入力'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <Button type='submit' variant='contained' color='secondary'>
+          送信
+        </Button>
+      </form>
       <button onClick={signOut}>ログアウト</button>
     </>
   );
