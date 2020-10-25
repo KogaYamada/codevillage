85fa4b92f819f5089a7d12f02e42e9de097a1196
KogaYamada <koga0087y@gmail.com>
2020-10-25 15:25:48 +0900

    10/25 33期生

--- 33/chatapp/src/context/AuthService.jsx
+++ 33/chatapp/src/context/AuthService.jsx
@@ -1,34 +1,32 @@
-import React, {useState, useEffect, createContext} from "react"
-import {auth} from "../firebase"
+import React, { useState, useEffect, createContext } from 'react';
+import { auth } from '../firebase';
 
 // コンテキストの作成
-const AuthContext = createContext()
+const AuthContext = createContext();
 
 // useEffect(関数, [依存変数/state])
 // 依存変数が変更されるたびに関数が実行される
 
-const AuthProvider = ({children}) => {
-	const [user, setUser] = useState(null);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
 
-	// 副作用を扱うためのhooks
-	// 関数のタイミング調整係
-	// 今回は画面描写後に一度だけ実行
-	useEffect(() => {
-		// ユーザーのログイン状態の変更を監視 => ログイン状態が切り替わるたびにcallbak関数にuser情報が渡されて実行される + 最初の1回
-		// 引数のuserはログイン状態ならユーザーのオブジェクト、ログアウト状態ならnullが入る
-		auth.onAuthStateChanged((user) => {
-			setUser(user)
-		})
-	},[])
+  // 副作用を扱うためのhooks
+  // 関数のタイミング調整係
+  // 今回は画面描写後に一度だけ実行
+  useEffect(() => {
+    // ユーザーのログイン状態の変更を監視 => ログイン状態が切り替わるたびにcallbak関数にuser情報が渡されて実行される + 最初の1回
+    // 引数のuserはログイン状態ならユーザーのオブジェクト、ログアウト状態ならnullが入る
+    auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+  }, []);
 
-	return (
-		<AuthContext.Provider value={ user }>
-			{children}
-		</AuthContext.Provider>
-	)
-}
+  return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
+};
 
-export {
-	AuthContext,
-	AuthProvider
-}
+export { AuthContext, AuthProvider };
+/*
+<AuthContext.Provider value={ user }>
+	<h1>hello</h1> // children
+</AuthContext.Provider>
+*/
--- 33/chatapp/src/pages/Room.jsx
+++ 33/chatapp/src/pages/Room.jsx
@@ -1,10 +1,95 @@
-import React, { useContext } from 'react';
-import {AuthContext} from "../context/AuthService"
-import {auth} from "../firebase"
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../context/AuthService';
+import { auth, db } from '../firebase';
+
+import { makeStyles } from '@material-ui/core/styles';
+import { Card, Button, TextField, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  message: {
+    padding: '15px',
+    margin: '15px',
+  },
+});
 
 const Room = () => {
-	const some = useContext(AuthContext)
-  return <div><button onClick={()=> auth.signOut()}>logout</button></div>;
+  const [messages, setMessages] = useState([]);
+  const [text, setText] = useState('');
+  const classes = useStyles();
+  const user = useContext(AuthContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    db.collection('messages').add({
+      content: text,
+      username: user.displayName,
+      createdAt: new Date(),
+    });
+  };
+
+  // useEffectのコールバック関数実行タイミング
+  //  - 第二引数の依存配列の中の変数が変わるたびにコールバック関数を実行する
+  //  - 画面が読み込まれた(描写された)あとの最初の一回実行される
+  useEffect(() => {
+    db.collection('messages').onSnapshot((querySnapshot) => {
+      const data = querySnapshot.docs.map((doc) => doc.data());
+      console.log(data);
+      setMessages(data);
+    });
+
+    // db.collection('messages')
+    //   .get()
+    //   .then((querySnapshot) => {
+    //     // setMessages(querySnapshot.docs.map((doc) => doc.data()));
+    //     const data = querySnapshot.docs.map((doc) => doc.data());
+    //     console.log(data);
+    //     setMessages(data);
+    //   });
+  }, []);
+
+  return (
+    <div>
+      {messages.map((message) => {
+        return (
+          <Card className={classes.message}>
+            <Typography>投稿者({message.username})</Typography>
+            <Typography>{message.content}</Typography>
+          </Card>
+        );
+      })}
+      <form onSubmit={handleSubmit}>
+        <TextField
+          variant='outlined'
+          size='small'
+          placeholder='メッセージを入力'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <Button type='submit' variant='contained' color='secondary'>
+          送信
+        </Button>
+      </form>
+      <button onClick={() => auth.signOut()}>logout</button>
+    </div>
+  );
 };
 
 export default Room;
+
+// SQL noSQL
+// callback : 関数の引数に関数をしていする
+/*
+const some = (fn) => {
+  fn();
+};
+
+some(() => {});
+
+const onClick = (fn) => {
+  const e = 'イベントの情報';
+  // クリックされた時
+  fn(e);
+};
+
+onClick((e)=>{e.target.value})
+*/
