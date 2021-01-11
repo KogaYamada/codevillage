ed08c8ebe75eeb46d08420457467b5faf1401c56
KogaYamada <koga0087y@gmail.com>
2021-01-11 20:19:21 +0900

    コード移行

---
+++ 51/main.js
@@ -0,0 +1,51 @@
+// ブラウザのコンソールにhello jsを表示
+// console.log('hello js');
+
+// 文字列: string
+// シングルクォーテーション or ダブルクォーテーション
+// or バッククォーテーションで囲われたもの
+
+// 数字: number
+// 浮動小数点数
+
+// 真偽値: boolean
+// 正しい(true) or 正しくない(false)
+// console.log(false);
+
+// null(値が存在しない)
+
+// undefiend(値が未定義)
+
+// コメント
+// これはコメントです
+
+/*ここから
+a
+b
+ここまで*/
+
+// 変数宣言
+// 値を格納するための箱
+
+// const: 再代入できない値を定義
+// let: 再代入できる値を定義
+
+// var: 古いから使わなくていい
+
+const number = 100;
+
+// 算術演算子
+
+// +, -, *, /
+// % 余り算
+
+const a = 10;
+const b = 2;
+const c = 3;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+
+console.log((1 + 2) * 10);
