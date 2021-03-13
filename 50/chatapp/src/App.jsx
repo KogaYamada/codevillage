c58349fbabcc40c2f2e004ac62ce08913d5bd413
KogaYamada <koga0087y@gmail.com>
2021-03-13 14:42:24 +0900

    3/13 50期せい

--- 50/chatapp/src/App.jsx
+++ 50/chatapp/src/App.jsx
@@ -2,22 +2,29 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Room from './pages/Room';
+import LoggedInRoute from './components/LoggedInRoute';
+
+import { AuthProvider } from './context/AuthContext';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path='/'>
-          <Room />
-        </Route>
-        <Route exact path='/login'>
-          <Login />
-        </Route>
-        <Route exact path='/signup'>
-          <Signup />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+            <LoggedInRoute>
+              <Room />
+            </LoggedInRoute>
+          </Route>
+          <Route exact path='/login'>
+            <Login />
+          </Route>
+          <Route exact path='/signup'>
+            <Signup />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </AuthProvider>
   );
 };
 
--- 50/chatapp/src/pages/Room.jsx
+++ 50/chatapp/src/pages/Room.jsx
@@ -1,5 +1,76 @@
+import { useContext, useEffect } from 'react';
+import { Card, Button, TextField, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { auth, db } from '../firebase/config';
+import { AuthContext } from '../context/AuthContext';
+
+const useStyles = makeStyles({
+  cardConteiner: {
+    backgroundColor: '#EEE',
+  },
+  card: {
+    width: '300px',
+    padding: '15px',
+    margin: '10px',
+  },
+});
+
 const Room = () => {
-  return <h1>チャットページ</h1>;
+  const classes = useStyles();
+  const state = useContext(AuthContext);
+  // firebase authからログアウトする関数
+  const logout = () => {
+    auth
+      .signOut()
+      .then(() => console.log('ログアウトしました'))
+      .catch((err) => console.log('ログアウトに失敗しました', err));
+  };
+
+  useEffect(() => {
+    db.collection('users')
+      .get()
+      .then((querySnapshot) => {
+        const data = querySnapshot.docs.map((doc) => doc.data());
+        console.log(data);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>チャットページ</h1>
+      <div className={classes.cardConteiner}>
+        <Card className={classes.card}>
+          <Typography>テストユーザー</Typography>
+          <Typography>こんにちは</Typography>
+        </Card>
+        <Card className={classes.card}>
+          <Typography>テストユーザー</Typography>
+          <Typography>こんにちは</Typography>
+        </Card>
+      </div>
+      <form>
+        <TextField variant='outlined' />
+        <Button variant='contained'>チャットを送信</Button>
+      </form>
+      <button onClick={logout}>ログアウト</button>
+    </>
+  );
 };
 
 export default Room;
+
+// const db = {
+//   users: {
+//     SvQA9ugMkcMRk8kE3zKX: {
+//       name: 'こうが',
+//       age: 12,
+//       createdAt: 'xx月xx日',
+//     },
+//     mH2XgQirSX1v5JMeHIdz: {
+//       name: 'たろう',
+//       age: 13,
+//       createdAt: 'xx月xx日',
+//     },
+
+//   },
+// };
